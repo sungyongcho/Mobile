3055b3e5e12eb06324ecc148422ee13cbda6702f
Sungyong Cho <dev@sungyongcho.com>
2024-11-12 23:50:40 +0100

    chore: upto #6; add, change fonts

--- tutorial/myapp/lib/main.dart
+++ tutorial/myapp/lib/main.dart
@@ -5,13 +5,24 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text('my first app'),
       centerTitle: true,
+      backgroundColor: Colors.red[600],
     ),
-    body: Center(
-      child: Text('hello world')
+    body: const Center(
+      child: Text(
+        'hello world',
+        style: TextStyle(
+			fontSize: 20.0,
+			fontWeight: FontWeight.bold,
+			letterSpacing: 2.0,
+			color: Color.fromARGB(255, 158, 158, 158),
+			fontFamily: 'Doto'
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
-      onPressed: () => (),
-      child: Text('btn')
+      onPressed: () {},
+      child: Text('btn'),
+      backgroundColor: Colors.red[500],
     ),
   ),
 ));
