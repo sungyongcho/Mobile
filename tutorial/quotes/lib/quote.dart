8758c78aa961b5910878ca5efaa111e56ea6c219
Sungyong Cho <dev@sungyongcho.com>
2024-11-14 00:02:39 +0100

    chore: upto #18; custom classes

---
+++ tutorial/quotes/lib/quote.dart
@@ -0,0 +1,8 @@
+class Quote {
+  String text;
+  String author;
+
+  Quote({required this.text, required this.author});
+}
+
+Quote myQuote = Quote(text: 'this is the quote text', author: 'oscar wilde');
