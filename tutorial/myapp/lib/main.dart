0916412b6428064d6af05c3c61ec3689f2dc15e7
Sungyong Cho <dev@sungyongcho.com>
2024-11-13 07:02:37 +0100

    chore: upto #8

--- tutorial/myapp/lib/main.dart
+++ tutorial/myapp/lib/main.dart
@@ -1,28 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('my first app'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: const Center(
-      child: Text(
-        'hello world',
-        style: TextStyle(
-			fontSize: 20.0,
-			fontWeight: FontWeight.bold,
-			letterSpacing: 2.0,
-			color: Color.fromARGB(255, 158, 158, 158),
-			fontFamily: 'Doto'
-        ),
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text('btn'),
-      backgroundColor: Colors.red[500],
-    ),
-  ),
-));
+      // body: Center(child: Image.asset('assets/image2.png')),
+      body: Center(
+          child: Image.network(
+              'https://i.ebayimg.com/images/g/w0sAAOSwBnFkn370/s-l1200.jpg')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('hi'),
+        backgroundColor: Colors.red[500],
+      ),
+    );
+  }
+}
