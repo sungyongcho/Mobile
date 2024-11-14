da6c2c533d8df7bd3a4e0456560369cd605440ed
Sungyong Cho <dev@sungyongcho.com>
2024-11-14 06:40:09 +0100

    chore: upto #30; passing route data

--- tutorial/world_time/lib/pages/home.dart
+++ tutorial/world_time/lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:core';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,8 +9,11 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Map data = {};
   @override
   Widget build(BuildContext context) {
+    data = ModalRoute.of(context)!.settings.arguments as Map;
+    print(data);
     return Scaffold(
       body: SafeArea(
         child: Column(
