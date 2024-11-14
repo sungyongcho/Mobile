f19955ffcdaab2d3837c37a1b0384245bae8c3d3
Sungyong Cho <dev@sungyongcho.com>
2024-11-14 02:42:03 +0100

    chore: upto #21; maps & routing

--- tutorial/world_time/lib/pages/choose_location.dart
+++ tutorial/world_time/lib/pages/choose_location.dart
@@ -11,6 +11,16 @@ class _ChooseLocationState extends State<ChooseLocation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text(
+          'Choose a location',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
       body: Text('choose location screen'),
     );
   }
