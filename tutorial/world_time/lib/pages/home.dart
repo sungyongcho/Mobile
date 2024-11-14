e99eb2d0de3088a93a9df8f1b5f23824cf4ec204
Sungyong Cho <dev@sungyongcho.com>
2024-11-14 13:49:25 +0100

    chore: done tutorial

--- tutorial/world_time/lib/pages/home.dart
+++ tutorial/world_time/lib/pages/home.dart
@@ -12,7 +12,9 @@ class _HomeState extends State<Home> {
   Map data = {};
   @override
   Widget build(BuildContext context) {
-    data = ModalRoute.of(context)!.settings.arguments as Map;
+    data = data.isNotEmpty
+        ? data
+        : ModalRoute.of(context)!.settings.arguments as Map;
     print(data);
 
     // set background
@@ -34,8 +36,17 @@ class _HomeState extends State<Home> {
             child: Column(
               children: <Widget>[
                 TextButton.icon(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/location');
+                  onPressed: () async {
+                    dynamic result =
+                        await Navigator.pushNamed(context, '/location');
+                    setState(() {
+                      data = {
+                        'time': result['time'],
+                        'location': result['location'],
+                        'flag': result['flag'],
+                        'isDaytime': result['isDaytime'],
+                      };
+                    });
                   },
                   icon: Icon(
                     Icons.edit_location,
